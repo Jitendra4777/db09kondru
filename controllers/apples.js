748d693e131afa81bcd49191fee526432160f666
Jitendra <jitendra.kondru@gmail.com>
2021-11-10 14:56:30 -0600

    added post

--- controllers/apples.js
+++ controllers/apples.js
@@ -46,4 +46,24 @@ exports.apple_view_all_Page = async function(req, res) {
         res.status(500); 
         res.send(`{"error": ${err}}`); 
     }   
-}; 
+};
+// Handle Costume create on POST. 
+exports.apple_create_post = async function(req, res) { 
+    console.log(req.body) 
+    let document = new apple(); 
+    // We are looking for a body, since POST does not have query parameters. 
+    // Even though bodies can be in many different formats, we will be picky 
+    // and require that it be a json object 
+    // {"costume_type":"goat", "cost":12, "size":"large"} 
+    document.costume_type = req.body.color; 
+    document.cost = req.body.cost; 
+    document.quantity = req.body.quantity; 
+    try{ 
+        let result = await document.save(); 
+        res.send(result); 
+    } 
+    catch(err){ 
+        res.status(500); 
+        res.send(`{"error": ${err}}`); 
+    }   
+};  
